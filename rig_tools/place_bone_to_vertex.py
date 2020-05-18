5abf4d196c41080f9f1e7b2e208486e2f453bb3c
Artem Poletsky <artempoletsky@gmail.com>
2020-05-18 22:28:00 +0500

    place bone to vert pair progress

--- rig_tools/place_bone_to_vertex.py
+++ rig_tools/place_bone_to_vertex.py
@@ -11,18 +11,19 @@ def find_armature(obj):
             armature = m.object
     return armature
 
-def place_bone_to_vert_pair(bone, v1, v2):
-    bone.head = v1
-    bone.tail = v2
+def place_bone_to_vert_pair(matrix, bone, v1, v2):
+    inv = matrix.inverted()
+    bone.head = inv @ v1
+    bone.tail = inv @ v2
 
-def place_bone_to_vert(bone, vert, orientation = 'NORMAL'):
+def place_bone_to_vert(matrix, bone, vert, orientation = 'NORMAL'):
     return
 
-def bone_distance_to_vert_pair(bone, v1, v2):
-    d1 = (bone.head - v1).length
-    d2 = (bone.tail - v2).length
-    d3 = (bone.head - v2).length
-    d4 = (bone.tail - v1).length
+def bone_distance_to_vert_pair(matrix, bone, v1, v2):
+    d1 = (matrix @ bone.head  - v1).length
+    d2 = (matrix @ bone.tail - v2).length
+    d3 = (matrix @ bone.head - v2).length
+    d4 = (matrix @ bone.tail - v1).length
     return min(d1 + d2, d3 + d4)
 
 def default_comparator(n1, n2):
@@ -33,12 +34,12 @@ def default_comparator(n1, n2):
     else:
         return -1
 
-def sort_closest_bones(bones, v1, v2):
+def sort_closest_bones(matrix, bones, v1, v2):
     if not v2:
         return bones
 
     def comparator(b1, b2):
-        return default_comparator(bone_distance_to_vert_pair(b1, v1, v2), bone_distance_to_vert_pair(b2, v1, v2))
+        return default_comparator(bone_distance_to_vert_pair(matrix, b1, v1, v2), bone_distance_to_vert_pair(matrix, b2, v1, v2))
 
     return sorted(bones, key = cmp_to_key(comparator), reverse = False)
 
@@ -84,16 +85,17 @@ class PlaceBoneToVertexOperator(bpy.types.Operator):
         armature = self.armature = find_armature(obj)
 
         bm = bmesh.from_edit_mesh(mesh)
-        selected_verts = [v.co for v in bm.verts if v.select]
-
+        matrix = obj.matrix_world
+        armature_matrix = armature.matrix_world
+        selected_verts = [matrix @ v.co for v in bm.verts if v.select]
         if len(selected_verts) == 2:
             v1, v2 = selected_verts
             self.select_armature(context)
             bones = self.armature.data.edit_bones
-            sorted_bones = sort_closest_bones(bones, v1, v2)
-            bone_name = sorted_bones[0].name
-            edit_bones = armature.data.edit_bones
-            place_bone_to_vert_pair(edit_bones[bone_name], v1, v2)
+            sorted_bones = sort_closest_bones(armature_matrix, bones, v1, v2)
+            bone = sorted_bones[0]
+            # bone_name = sorted_bones[0].name
+            place_bone_to_vert_pair(armature_matrix, bone, v1, v2)
             self.select_object(context)
         bm.free()
         return {'FINISHED'}
