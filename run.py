263a91c965cb41e2e7fc0f9417ef7fdf0543c642
Artem Poletsky <artempoletsky@gmail.com>
2020-09-20 23:50:54 +0500

    renaming project

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ blend_dir = os.path.dirname(bpy.data.filepath)
 if blend_dir not in sys.path:
    sys.path.append(blend_dir)
 
-import rig_tools
+import one_click_rig
 #import single_file_addon
 import importlib
-importlib.reload(rig_tools)
+importlib.reload(one_click_rig)
 #imp.reload(single_file_addon)
 rig_tools.register()
 #single_file_addon.register()
